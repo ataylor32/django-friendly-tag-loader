00b91090172433b04f90c850bb1edeed1a262abf
Adam Taylor <ataylor32@gmail.com>
2021-12-11 15:15:04 -0700

    Also test with Django 4.0

--- tests/testproject/urls.py
+++ tests/testproject/urls.py
@@ -1,7 +1,11 @@
-from django.conf.urls import url
 from django.shortcuts import render
 
+try:
+    from django.urls import re_path
+except ImportError:
+    # Django 1.11
+    from django.conf.urls import url as re_path
 
 urlpatterns = [
-    url(r'$', lambda request: render(request, 'test.html'))
+    re_path(r'$', lambda request: render(request, 'test.html'))
 ]
